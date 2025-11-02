354c68af63b7131f2cee0b2ed927f0cd19cbb6ea
Rudolf <rudolf@MacBookAir.lan>
2025-11-02 23:56:29 +0100

    feat: Implemented One-to-many relationship (Workout to Exercise) and finalized CRUD migration to SQLAlchemy.

--- gym_tracker.py
+++ gym_tracker.py
@@ -1,27 +1,141 @@
 from models import Workout, GymTracker
-from datetime import  datetime
+from datetime import datetime
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     tracker = GymTracker()
     print("Welcome to your Gym Tracker")
     while True:
-        option = input(f"Select an option: \n 1. Add workout \n 2. Show workouts \n 3. Exit \n")
+        option = input(
+            f"Select an option: \n 1. Add workout \n 2. Show workouts \n 3. Delete workout \n 4. Edit "
+            f"workout \n 5. Exit \n"
+        )
         if option == "1":
             workout_name = input("Enter your workout name: ")
             workout_date = input("Enter your workout date (YYYY-MM-DD): ")
             workout_duration = input("Enter your workout duration (in minutes): ")
+
             try:
-                datetime.strptime(workout_date, '%Y-%m-%d')
+                datetime.strptime(workout_date, "%Y-%m-%d")
                 duration_float = float(workout_duration)
-                new_workout = Workout(workout_date, workout_name, duration_float)
-                tracker.add_workout(new_workout)
-                print(f"Workout '{workout_name}' has been successfully added.")
+
+                workout_data = (workout_date, workout_name, duration_float)
+
+                workout_id, workout_name_added = tracker.add_workout(workout_data)
+
+                print(
+                    f"Workout '{workout_name_added}' has been successfully added. Now please add exercises to your workout."
+                )
+
+                while True:
+                    exercise_name = input("  Exercise name: ")
+                    if not exercise_name:
+                        print("Adding exercise cancelled.")
+                        break
+                    try:
+                        ex_sets = int(input("  Number of sets: "))
+                        ex_reps = int(input("  Number of reps: "))
+                        ex_weight = float(input("  Total weight (kg): "))
+
+                        tracker.add_exercise(
+                            workout_id=workout_id,
+                            name=exercise_name,
+                            sets=ex_sets,
+                            reps=ex_reps,
+                            weight=ex_weight,
+                        )
+                        print(f"  Exercise '{exercise_name}' added.")
+
+                    except ValueError:
+                        print(
+                            "Error: Sets and reps must be an Integer. Weight must be a float."
+                        )
+                        continue
+
+                    if (
+                        input("Do you want to add another exercise? (y/n): ").lower()
+                        != "y"
+                    ):
+                        break
+
             except ValueError:
-                print("Error: Please enter a valid date and make sure duartion is a valid number.")
+                print(
+                    "Error: Make sure date has YYYY-MM-DD format and duration is a float."
+                )
+            except Exception as e:
+                print(f"Error while adding workout: {e}")
+
         elif option == "2":
             tracker.show_workouts()
         elif option == "3":
+            tracker.show_workouts()
+
+            try:
+                workout_id_str = input("Choose a workout to delete: ")
+                if not workout_id_str:
+                    print("Deleting cancelled.")
+                    continue
+                workout_id = int(workout_id_str)
+                is_deleted = tracker.delete_workout(workout_id)
+                if is_deleted:
+                    print(f"Workout {workout_id} successfully deleted")
+                else:
+                    print(f"Error: Workout {workout_id} has not been found.")
+
+            except ValueError:
+                print("Error: Please choose a valid workout ID.")
+
+        elif option == "4":
+            num_workouts = tracker.show_workouts()
+
+            if num_workouts == 0:
+                continue
+
+            try:
+                workout_id_str = input("Choose a workout ID to edit: ")
+                workout_id = int(workout_id_str)
+                old_workout_obj = tracker.get_workout_by_id(workout_id)
+                if not old_workout_obj:
+                    print(f"Error: Workout {workout_id} does not exists.")
+                    continue
+                print("\n Editing workout")
+
+                new_name = input(
+                    f"New name (recent: {old_workout_obj.name}). Leave blank not to change: "
+                )
+                new_name = new_name if new_name else old_workout_obj.name
+
+                new_date = input(
+                    f"New date (recent: {old_workout_obj.workout_date}). Leave blank not to change: "
+                )
+                new_date = new_date if new_date else old_workout_obj.workout_date
+
+                new_duration_str = input(
+                    f"New duration (recent: {old_workout_obj.duration}). Leave blank not to change"
+                )
+                if new_duration_str:
+                    new_duration_str = float(new_duration_str)
+                else:
+                    new_duration = old_workout_obj.duration
+
+                if new_date != old_workout_obj.workout_date:
+                    datetime.strptime(new_date, "%Y-%m-%d")
+
+                is_updated = tracker.update_workout(
+                    workout_id, new_name, new_date, new_duration
+                )
+
+                if is_updated:
+                    print(f"Workout {workout_id} successfully updated.")
+                else:
+                    print("Error: Database did not answer.")
+
+            except ValueError:
+                print("Error: Make sure....")
+
+            except Exception as e:
+                print(f"Unknown error occurred: {e}")
+
+        elif option == "5":
             print("Exiting Gym Tracker. Goodbye!")
             break
         else:
--- models.py
+++ models.py
@@ -1,29 +1,186 @@
+from traceback import print_tb
+
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, foreign, relationship
 from datetime import datetime
 
 
-class Workout:
-    """Class for a single workout"""
+engine = create_engine("sqlite:///tracker.db")
 
-    def __init__(self, date, name, duration):
-        self.date: datetime = date
-        self.name: str = name
-        self.duration: float = duration
+Base = declarative_base()
 
-    def __str__(self):
-        return f"Training: {self.name} | Date: {self.date} | Duration: {self.duration} min"
+
+class Exercise(Base):
+    __tablename__ = "exercises"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    sets = Column(Integer)
+    reps = Column(Integer)
+    weight = Column(Float)
+
+    workout_id = Column(Integer, ForeignKey("workouts.id"))
+
+    def __repr__(self):
+        return f"<Exercise(name='{self.name}', sets={self.sets}, weight={self.weight})>"
+
+
+class Workout(Base):
+    __tablename__ = "workouts"
+    id = Column(Integer, primary_key=True)
+    workout_date = Column(String)
+    name = Column(String)
+    duration = Column(Float)
+
+    exercises = relationship(
+        "Exercise", backref="workout", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"<Workout(id={self.id}, name={self.name}, duration={self.duration})>"
+
+
+Session = sessionmaker(bind=engine)
 
 
 class GymTracker:
     def __init__(self):
-        self.training_base: list = []
+        self.Session = sessionmaker(bind=engine)
+        Base.metadata.create_all(engine)
 
-    def add_workout(self, workout):
-        self.training_base.append(workout)
+    def add_workout(self, workout_data):
+        session = self.Session()
+        try:
+            new_workout = Workout(
+                workout_date=workout_data[0],
+                name=workout_data[1],
+                duration=workout_data[2],
+            )
+
+            session.add(new_workout)
+            session.commit()
+
+            workout_id = new_workout.id
+            workout_name = new_workout.name
+
+            session.close()
+
+            return workout_id, workout_name
+
+        except Exception as e:
+            session.rollback()
+            session.close()
+            raise e
+
+    def add_exercise(
+        self, workout_id: int, name: str, sets: int, reps: int, weight: float
+    ):
+        session = self.Session()
+
+        try:
+            workout = session.get(Workout, workout_id)
+
+            if workout:
+                new_exercise = Exercise(name=name, sets=sets, reps=reps, weight=weight)
+
+                workout.exercises.append(new_exercise)
+
+                session.commit()
+                return True
+            return False
+
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
 
     def show_workouts(self):
-        print("\n Your workouts")
-        if not self.training_base:
-            print("No workouts added yet. Please add a workout.")
-            return
-        for idx, workout in enumerate(self.training_base, 1):
-            print(f"\n {idx}. {workout}")
+        session = self.Session()
+
+        try:
+            workouts = session.query(Workout).order_by(Workout.id.desc()).all()
+
+            if not workouts:
+                print("No saved workouts.")
+                return 0
+            else:
+                print("\n Your saved workouts")
+            for workout in workouts:
+                print(
+                    f"\n[ID: {workout.id}] Date: {workout.workout_date} | Name: {workout.name} | Duration: {workout.duration} min"
+                )
+
+                if workout.exercises:
+                    print("  Exercises:")
+                    for exercise in workout.exercises:
+                        print(
+                            f"      - {exercise.name}: {exercise.sets} sets, {exercise.reps} reps, {exercise.weight} kg"
+                        )
+                else:
+                    print("    (No saved exercises for that workout.)")
+            return len(workouts)
+
+        except Exception as e:
+            print(f"Error while showing workouts: {e}")
+            return 0
+
+        finally:
+            session.close()
+
+    def delete_workout(self, workout_id):
+        session = self.Session()
+        is_deleted = False
+
+        try:
+            workout_to_delete = session.get(Workout, workout_id)
+            if workout_to_delete:
+                session.delete(workout_to_delete)
+                session.commit()
+                is_deleted = True
+
+        except Exception as e:
+            session.rollback()
+            print(f"Error while deleting workout: {e}")
+
+        finally:
+            session.close()
+        return is_deleted
+
+    def get_workout_by_id(self, workout_id: int):
+        session = self.Session()
+
+        try:
+            workout = session.get(Workout, workout_id)
+            return workout
+
+        except Exception as e:
+            print(f"Error while editing workout: {e}")
+            return None
+
+        finally:
+            session.close()
+
+    def update_workout(
+        self, workout_id: int, new_name: str, new_date: str, new_duration: float
+    ) -> bool:
+        session = self.Session()
+        is_updated = False
+
+        try:
+            workout = session.get(Workout, workout_id)
+
+            if workout:
+                workout.name = new_name
+                workout.workout_date = new_date
+                workout.duration = new_duration
+
+                session.commit()
+                is_updated = True
+
+        except Exception as e:
+            session.rollback()
+            print(f"Error while updating workout: {e}")
+
+        finally:
+            session.close()
+        return is_updated
